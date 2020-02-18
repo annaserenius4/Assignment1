c1c6a6dff921b8789d88516044691c0546f1c8ee
annaserenius4 <59447269+annaserenius4@users.noreply.github.com>
2020-02-18 17:50:46 -0500

    Update BacktrackingComparator.java

--- src/BacktrackingSearch/BacktrackingComparator.java
+++ src/BacktrackingSearch/BacktrackingComparator.java
@@ -4,10 +4,16 @@ import java.util.Comparator;
 
 public class BacktrackingComparator implements Comparator<BacktrackingNode>{//to be used with a priority queue that orders nodes from most to least constrained
 
-        public int compare(BacktrackingNode backtrackingNode1, BacktrackingNode backtrackingNode2) {
+     public int compare(BacktrackingNode backtrackingNode1, BacktrackingNode backtrackingNode2) {
 
+             //Most constrained variable
             if(backtrackingNode1.domain.size() > backtrackingNode2.domain.size()) return 1;
             if(backtrackingNode1.domain.size() < backtrackingNode2.domain.size()) return -1;
+            else{
+                    //for a tie, then do Most constraining variable
+                if(backtrackingNode1.arcs.size() < backtrackingNode2.arcs.size()) return 1;
+                if(backtrackingNode1.arcs.size() > backtrackingNode2.arcs.size()) return -1;
+            }
             return 0;
         }
 
